dab52baa571581bd215334c04ffa75f770d2477e
Crpearce <Crpearce892@gmail.com>
2023-06-22 09:06:55 -0600

    update nav

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom'
 
+import banner from '../../assets/banner.jpg'
+
 import './navigation.styles.css'
 
 const Navigation = () => {
   return (
     <nav className='nav-container'>
-        <h1>2023 Dead Tour Tracker</h1>
+        <img src={banner} style={{width: '40%'}}/>
       <div className='nav-links-container'>
-      <Link to='/'>Home
+      <Link to='/' style={{textDecoration: 'none', color: '#171515'}}>Home
       </Link>
-        <Link to='/song-tracker'>
+        <Link to='/song-tracker' style={{textDecoration: 'none', color: '#171515'}}>
           Song Tracker
         </Link>
       </div>
