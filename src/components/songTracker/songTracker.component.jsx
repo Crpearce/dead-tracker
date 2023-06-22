deb1e8d1760c4756f141cc1871f1897e74b308b1
Crpearce <Crpearce892@gmail.com>
2023-06-22 09:53:27 -0600

    reduce function for all dates song was played

--- src/components/songTracker/songTracker.component.jsx
+++ src/components/songTracker/songTracker.component.jsx
@@ -3,8 +3,28 @@ import './songTracker.styles.css'
 import { concerts } from '../../data'
 
 const SongTracker = () => {
-//   const songCount = locations.reduce((acc, curr) => {}, [])
-console.log(concerts)
+
+  // iterate over each concert, spread both set1 and set 2 together?  Each song needs to be paired with the concert date,
+  // the song will be the key here, as each time it is played, we will add the new date played into the acc
+  // [{title: 'truckin', dates: ['6/22/21', ]}]
+  const songCount = concerts.reduce((acc, curr) => {
+      curr.set1.forEach(song => {
+        if(!acc[song]) {
+          acc[song] = []
+        } 
+        acc[song].push(curr.date)
+      })
+      curr.set2.forEach(song => {
+        if(!acc[song]) {
+          acc[song] =[]
+        }
+        acc[song].push(curr.date)
+      })
+      console.log(acc)
+      return acc
+      
+  }, [])
+
   return (
     <div>
       <h1>songs</h1>
